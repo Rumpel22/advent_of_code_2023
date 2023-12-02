e4cd53dc39e0e65c037f48053365bd651fc37eff
Pascal Wey <pascal@pfft.ch>
2023-12-02 09:41:16 +0000

    Solution day 1, part 1

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,20 @@
+fn get_digits(line: &str) -> Vec<u32> {
+    line.chars().filter_map(|c| c.to_digit(10)).collect()
+}
+
 fn main() {
-    let input = include_str!("../data/demo_input.txt");
+    let input = include_str!("../data/input.txt");
+
+    let sum = input
+        .lines()
+        .map(|line| {
+            let digits = get_digits(line);
+            let first_digit = digits.first().unwrap();
+            let second_digit = digits.last().unwrap();
+
+            first_digit * 10 + second_digit
+        })
+        .sum::<u32>();
+
+    println!("The sum of the arabic numbers is {sum}.");
 }
