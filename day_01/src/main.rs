8936a5758ff2c3b8450372efbe21a60a0830cb6f
Pascal Wey <pascal@pfft.ch>
2023-12-02 14:46:04 +0000

    Solution day 1, part 2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,5 +1,39 @@
-fn get_digits(line: &str) -> Vec<u32> {
-    line.chars().filter_map(|c| c.to_digit(10)).collect()
+const WORDS: [(&str, u32); 9] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
+fn get_first_digit(line: &str) -> Option<u32> {
+    for (word, value) in WORDS {
+        if line.starts_with(word) {
+            return Some(value);
+        }
+    }
+    if line.chars().next().unwrap().is_ascii_digit() {
+        line.chars().next().unwrap().to_digit(10)
+    } else {
+        get_first_digit(&line[1..])
+    }
+}
+
+fn get_last_digit(line: &str) -> Option<u32> {
+    for (word, value) in WORDS {
+        if line.ends_with(word) {
+            return Some(value);
+        }
+    }
+    if line.chars().last().unwrap().is_ascii_digit() {
+        line.chars().last().unwrap().to_digit(10)
+    } else {
+        get_last_digit(&line[..line.len() - 1])
+    }
 }
 
 fn main() {
@@ -8,13 +42,12 @@ fn main() {
     let sum = input
         .lines()
         .map(|line| {
-            let digits = get_digits(line);
-            let first_digit = digits.first().unwrap();
-            let second_digit = digits.last().unwrap();
+            let first_digit = get_first_digit(line).unwrap();
+            let last_digit = get_last_digit(line).unwrap();
 
-            first_digit * 10 + second_digit
+            first_digit * 10 + last_digit
         })
         .sum::<u32>();
 
-    println!("The sum of the arabic numbers is {sum}.");
+    println!("The sum of the numbers is {sum}.");
 }
