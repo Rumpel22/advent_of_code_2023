dcb7efe4a2ed54c99df007ade53ac76e6a393dfc
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2023-12-10 10:05:11 +0000

    Day 4, part 2

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -1,13 +1,13 @@
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 #[derive(Debug)]
-struct Game {
+struct Card {
     id: u32,
     winning_numbers: Vec<u32>,
     having_numbers: Vec<u32>,
 }
 
-impl Game {
+impl Card {
     fn points(&self) -> u32 {
         match self.number_of_wins() as u32 {
             0 => 0,
@@ -23,7 +23,7 @@ impl Game {
     }
 }
 
-impl FromStr for Game {
+impl FromStr for Card {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -39,7 +39,7 @@ impl FromStr for Game {
             .split_ascii_whitespace()
             .map(|s| s.trim().parse::<u32>().unwrap())
             .collect::<Vec<_>>();
-        Ok(Game {
+        Ok(Card {
             id,
             winning_numbers,
             having_numbers,
@@ -48,12 +48,45 @@ impl FromStr for Game {
 }
 
 fn main() {
-    let input = include_str!("../data/demo_input.txt");
-    let games = input
+    let input = include_str!("../data/input.txt");
+    let cards = input
         .lines()
-        .map(|line| line.parse::<Game>().unwrap())
+        .map(|line| line.parse::<Card>().unwrap())
         .collect::<Vec<_>>();
 
-    let total_points = games.iter().map(|game| game.points()).sum::<u32>();
+    let total_points = cards.iter().map(|card| card.points()).sum::<u32>();
     println!("In total, there are {} points.", total_points);
+
+    let max_card_id = cards
+        .iter()
+        .max_by(|card1, card2| card1.id.cmp(&card2.id))
+        .map(|card| card.id)
+        .unwrap();
+
+    let mut card_total = cards
+        .iter()
+        .map(|card| (card.id, 1))
+        .collect::<HashMap<_, _>>();
+
+    for card_id in 1..=max_card_id {
+        let card = cards
+            .iter()
+            .filter(|card| card.id == card_id)
+            .next()
+            .unwrap();
+        let wins = card.number_of_wins();
+        let card_amount = *card_total.get(&card_id).unwrap();
+
+        for win in card_id + 1..=card_id + (wins as u32) {
+            if let Some(x) = card_total.get_mut(&win) {
+                *x = *x + card_amount;
+            }
+        }
+    }
+
+    let total_number_of_cards = card_total.iter().map(|(_, count)| count).sum::<usize>();
+    println!(
+        "At the end, there are {} scratchcards.",
+        total_number_of_cards
+    );
 }
