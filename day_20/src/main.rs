421def501459a5d7496412c248caf3b52d2612fa
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2024-06-23 15:30:38 +0000

    Solution day 20, part 2 (sort of...)

--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -11,10 +11,14 @@ fn main() {
     let mut signals = VecDeque::new();
     let mut signal_count = (0usize, 0usize);
     for _ in 0..1000 {
+        // for button_presses in 1..=10000 {
         signals.push_back(("button", Signal::Low, "broadcaster"));
 
         while !signals.is_empty() {
             let (sender, signal, current_target) = signals.pop_front().unwrap();
+            // if sender == "th" && signal == Signal::High {
+            //     println!("{}", button_presses);
+            // }
             match signal {
                 Signal::High => signal_count.1 += 1,
                 Signal::Low => signal_count.0 += 1,
@@ -38,4 +42,11 @@ fn main() {
         "Multiplied, the result is {}.",
         signal_count.0 * signal_count.1
     );
+    // ========================
+
+    // Part 2:
+    // The "rx"-module is solely dependent on module "cn", which in turn is a conjunction with inputs "th", "sv", "gh" and "ch".
+    // "cn" emits a low signal when all of its four inputs are high. The inputs become high after 3947, 4001, 3943 and 3917 button
+    // presses, respectivally. The least common multiple (lcm) of these four numbers is 243902373381257, which is accepted as solution.
+    // See commented code above how these four numbers has been found.
 }
