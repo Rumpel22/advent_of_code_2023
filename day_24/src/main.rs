56fc7cd3dccd7036550a7120353b4c105cbbd060
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2024-08-11 14:50:53 +0000

    Day 24, part 1

---
+++ day_24/src/main.rs
@@ -0,0 +1,80 @@
+use itertools::Itertools;
+use regex::Regex;
+use std::{ops::RangeInclusive, str::FromStr};
+use vector3d::Vector3d;
+
+struct Hailstone {
+    position: Vector3d<f64>,
+    velocity: Vector3d<f64>,
+}
+
+impl FromStr for Hailstone {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let rx = Regex::new(r"(-?\d+)").unwrap();
+        let mut matches = rx.find_iter(input);
+
+        let x = matches.next().unwrap().as_str().parse().unwrap();
+        let y = matches.next().unwrap().as_str().parse().unwrap();
+        let z = matches.next().unwrap().as_str().parse().unwrap();
+        let vx = matches.next().unwrap().as_str().parse().unwrap();
+        let vy = matches.next().unwrap().as_str().parse().unwrap();
+        let vz = matches.next().unwrap().as_str().parse().unwrap();
+
+        Ok(Hailstone {
+            position: Vector3d::new(x, y, z),
+            velocity: Vector3d::new(vx, vy, vz),
+        })
+    }
+}
+
+fn parse_hailstones(input: &str) -> Vec<Hailstone> {
+    input
+        .lines()
+        .map(|line| Hailstone::from_str(line).unwrap())
+        .collect::<Vec<_>>()
+}
+
+fn do_intersect(h1: &Hailstone, h2: &Hailstone, range: &RangeInclusive<f64>) -> bool {
+    let a1 = h1.velocity.y / h1.velocity.x;
+    let b1 = h1.position.y - a1 * h1.position.x;
+    let a2 = h2.velocity.y / h2.velocity.x;
+    let b2 = h2.position.y - a2 * h2.position.x;
+
+    if a1 == a2 {
+        return b1 == b2;
+    }
+    let x = (b2 - b1) / (a1 - a2);
+    let y = a1 * x + b1;
+    if !range.contains(&x) || !range.contains(&y) {
+        return false;
+    }
+    let t1 = (x - h1.position.x) / h1.velocity.x;
+    let t2 = (x - h2.position.x) / h2.velocity.x;
+    if t1 < 0.0 || t2 < 0.0 {
+        return false;
+    }
+    true
+}
+
+fn count_intersections(hailstones: &[Hailstone], range: &RangeInclusive<f64>) -> usize {
+    hailstones
+        .iter()
+        .combinations(2)
+        .map(|pair| do_intersect(pair[0], pair[1], range))
+        .filter(|b| *b)
+        .count()
+}
+
+fn main() {
+    let input = include_str!("../data/input.txt");
+    let hailstones = parse_hailstones(input);
+
+    // let intersections = count_intersections(&hailstones, &(7.0..=27.0));
+    let intersections = count_intersections(&hailstones, &(200000000000000.0..=400000000000000.0));
+    println!(
+        "There are {} possible intersections in the area",
+        intersections
+    );
+}
